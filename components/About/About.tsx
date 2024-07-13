10b864079cb3526e027f1270287d75d6e4b1a6c5
Robert <robert@hokei.com>
2024-07-13 20:08:05 +0800

    Update Carousel

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,29 +1,16 @@
 import { useState } from 'react';
-
+import AboutJson from 'json/About.json'
 const About = () => {
- 
+    console.log(AboutJson);
     return (
         <div className="About">
-About Legitem
-<br></br>
-Welcome to Legitem, your ultimate destination for stylish and reliable fashion essentials. Established in 2024 in Olongapo, we are committed to providing our customers with high-quality products that blend comfort, affordability, and contemporary style.
-<br></br>
-Our Mission
-<br></br>
-At Legitem, our mission is simple: to offer fashion-forward individuals a curated selection of clothing and accessories that complement their lifestyle. Whether you're looking for everyday basics, standout pieces for special occasions, or trendy accessories to complete your look, we've got you covered.
-<br></br>
-Why Choose Legitem?
-<br></br>
-Quality Assurance: We source our products from trusted suppliers to ensure durability and comfort without compromising on style.
-<br></br>
-Affordable Fashion: We believe that looking good shouldn't break the bank. That's why we strive to offer competitive prices and regular promotions to make fashion accessible to everyone.
-<br></br>
-Customer Satisfaction: Your satisfaction is our priority. We're dedicated to providing excellent customer service and a seamless shopping experience from start to finish.
-<br></br>
-Connect With Us
-<br></br>
-Follow us on social media to stay updated on the latest trends, promotions, and new arrivals. Join the Legitem community and be inspired to express your unique sense of style with confidence.
-  
+            <div>{AboutJson[0].Name}</div>
+            <div>{AboutJson[0].Description}</div>
+            <div>{AboutJson[0].Mission}</div>
+            <div>{AboutJson[0]["Why_Choose_Legitem"].Quality_Assurance}</div>
+            <div>{AboutJson[0]["Why_Choose_Legitem"].Affordable_Fashion}</div>
+            <div>{AboutJson[0].Connect_With_Us}</div>
+            <div>{AboutJson[0].Name}</div>
         </div>
     );
 };
--- components/About/Disclaimer.tsx
+++ components/About/Disclaimer.tsx
@@ -1,39 +1,13 @@
 import React from 'react'
-
+import DisclaimerJson from 'json/Disclaimer.json'
 const Disclaimer = () => {
   return (
     <div>
-          1. General Disclaimer
-                <br></br>
-
-The information provided on the Legitem website [www.legitem.com] (the "Site") is for general informational purposes only. All information on the Site is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability, or completeness of any information on the Site.
-<br></br>
-
-2. External Links Disclaimer
-<br></br>
-
-The Site may contain links to external websites that are not provided or maintained by or in any way affiliated with Legitem. Please note that we do not guarantee the accuracy, relevance, timeliness, or completeness of any information on these external websites.
-<br></br>
-
-3. Professional Advice Disclaimer
-<br></br>
-
-The information provided on the Site is not intended to constitute legal, financial, medical, or professional advice of any kind. You should seek appropriate professional advice before taking any action based on any information contained on the Site.
-<br></br>
-
-4. Limitation of Liability
-<br></br>
-
-In no event shall Legitem, nor any of its officers, directors, and employees, be liable to you for anything arising out of or in any way connected with your use of the Site, whether such liability is under contract, tort, or otherwise, and Legitem, including its officers, directors, and employees shall not be liable for any indirect, consequential, or special liability arising out of or in any way related to your use of the Site.
-<br></br>
-
-5. Changes to This Disclaimer
-<br></br>
-
-We may update this disclaimer from time to time in order to reflect, for example, changes to our practices or for other operational, legal, or regulatory reasons. We will notify you of any changes by posting the new disclaimer on the Site. You are advised to review this disclaimer periodically for any changes.
-
-
-
+        <div>{DisclaimerJson['General_Disclaimer'].content}</div>
+        <div>{DisclaimerJson['External_Links_Disclaimer'].content}</div>
+        <div>{DisclaimerJson['Professional_Advice_Disclaimer'].content}</div>
+        <div>{DisclaimerJson['Limitation_of_Liability'].content}</div>
+        <div>{DisclaimerJson['Changes_to_This_Disclaimer'].content}</div>
     </div>
   )
 }
