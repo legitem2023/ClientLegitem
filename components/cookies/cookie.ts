ac097bccd99d58ecdd5f6efc5d7dbc8d91690ff0
Robert <robert@hokei.com>
2024-08-25 19:53:27 +0800

    Update UI 3

--- components/cookies/cookie.ts
+++ components/cookies/cookie.ts
@@ -1,36 +1,47 @@
-import jwt from 'jsonwebtoken'
-import { setGlobalState } from 'state'
-interface JwtPayload {
+import jwt, { JwtPayload as DefaultJwtPayload } from 'jsonwebtoken';
+import { setGlobalState } from 'state';
+
+interface JwtPayload extends DefaultJwtPayload {
     user: {
-        jsonToken: string,
-        statusText: string
-    },
-    exp: any
+        id: string;
+        emailAddress: string;
+        userLevel: string;
+    };
 }
+
 export const cookies = () => {
-    const getCookie = (cookieName: any) => {
+    const getCookie = (cookieName: string): string | null => {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i].trim();
-            if (cookie.startsWith(cookieName + '=')) {
+            if (cookie.startsWith(`${cookieName}=`)) {
                 return cookie.substring(cookieName.length + 1);
             }
         }
         return null;
     }
+
     const cookie = getCookie("token");
-    let token: any = jwt.decode(cookie) as JwtPayload;
-    if (token === null || token === undefined) document.location.href = '../Management';
+
+    if (!cookie) {
+        document.location.href = '../Management';
+        return;
+    }
+
+    const token = jwt.decode(cookie) as JwtPayload | null;
+
+    if (!token || !token.user) {
+        document.location.href = '../Management';
+        return;
+    }
+
     setGlobalState("cookieEmailAddress", token.user.emailAddress);
     setGlobalState("cookieUserLevel", token.user.userLevel);
     setGlobalState("cookieActiveUser", token.user.id);
+
     return {
         id: token.user.id,
         email: token.user.emailAddress,
         userlevel: token.user.userLevel,
-    }
+    };
 }
-
-
-
-
