d0f03fdc1d7026ef088bb6f8424e00096f7fbad5
Robert <robert@hokei.com>
2024-09-25 11:47:23 +0800

    update 2

--- components/Crowd/CrowdMessages.tsx
+++ components/Crowd/CrowdMessages.tsx
@@ -8,11 +8,12 @@ import { Icon } from '@iconify/react'
 
 const CrowdMessages = () => {
     const [drawerState] = useGlobalState("drawer");
+    const [userEmail] = useGlobalState("cookieEmailAddress") 
 
     return (
         <div className='body_messages'>
             <div className={`${drawerState ? 'LeftWing' : 'LeftWing_'}`}>
-                <ActiveUsers/>
+            <ActiveUsers email={userEmail}/>
             </div>
             <div className='middlecontainer_messages'>
             <div className='LabelHead carouselLabel'><Icon icon="jam:messages-f" />Crowd Messages</div>
