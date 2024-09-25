7d4265bc968edca324e9416b12d4635e42734f43
Robert <robert@hokei.com>
2024-09-25 11:44:24 +0800

    update

--- components/Crowd/ActiveUsers.tsx
+++ components/Crowd/ActiveUsers.tsx
@@ -1,13 +1,20 @@
-import { useSubscription } from '@apollo/client'
-import { ACTIVE_USERS } from 'graphql/subscriptions'
-
+import {  useQuery, useSubscription } from '@apollo/client'
+import { READ_ACTIVE_USER } from 'graphql/queries';
+import { GROUP_SENDER } from 'graphql/queries';
+import { ACTIVE_USERS } from 'graphql/subscriptions';
 import { setGlobalState, useGlobalState } from 'state';
 
-const ActiveUsers = () => {
-  const {data,loading} = useSubscription(ACTIVE_USERS)
+const ActiveUsers = ({email}) => {
+
+  const {data:Userdata,loading:Userloading} = useQuery(GROUP_SENDER,{
+    variables:{
+      emailAddress:email
+    }
+  })
+
   const [ActiveUsers]:any = useGlobalState("cookieArray");
-  if(loading) return
-  if(ActiveUsers===undefined) return
+  if(Userloading) return
+
   const drawer = (data:any) =>{
     setGlobalState("drawer",true);
     setGlobalState("SelectedReciever",data);
@@ -15,8 +22,8 @@ const ActiveUsers = () => {
   return (
     <ul className='Menu'>
     <li className='Menu_label'>Active Users</li>
-      {ActiveUsers.length > 0?ActiveUsers?.map((item: any, index: any) => (
-      <li key={index} className='menu_li' onClick={()=>drawer(item.accountEmail)}>
+    {ActiveUsers.length > 0?ActiveUsers?.map((item: any, index: any) => (
+      <li key={index} className='menu_li' onClick={()=>drawer(item.accountEmail)} style={{display:item.accountEmail===email?"none":"block"}}>
         {item.accountEmail}
       </li>
     )):<li>No Available Users</li>}
